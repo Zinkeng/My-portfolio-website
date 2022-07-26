1d87787d229e27cfac9f405f51d53f7ab42f5fb0
Zinkeng <ajongafaczinkeng@gmail.com>
2022-07-27 00:55:03 +0100

    Worked on the aboutme page

--- src/Home.js
+++ src/Home.js
@@ -10,9 +10,27 @@ const Home = () => {
                     <p>Graphic designer & Software Developer</p>   
                 </div>
             </div>
-            <div>
-                      
-                </div>
+            <div className="aboutpage">
+                <div className="aboutme">
+                <p>Discover</p>
+                
+               </div>
+                <div className="container">
+              
+                    <p>About me{'\n'}
+                    My name is Ajongafac Zinkeng Mbetem. I am a Software engineer aspirant and graphic designer, based in Buea, Cameroon. I’m very passionate and dedicated to my work. With my three years experience in engineering school, I have acquired the skills necessary to build great and premuim websites, applications and softwares. I am particularly interested in open souce community projects and projects that brings about innovation in the Cameroon tech space and beyond.</p>
+                    <img src={require("./Anne2.JPG")} alt="Zinkeng"/>
+                    <ul>
+                        <li>Name: Ajongafac Zinkeng Mbetem</li>
+                        <li>Age: 20 years</li>
+                        <li>Address: Buea, Cameroon</li>
+                        <li>Phone: (+237) 682743056</li>
+                        <li>E-mail: ajongafaczinkeng@gmail.com</li>
+                        <li>Github link: https://github.com/Zinkeng</li>
+                    </ul>
+                </div> 
+            </div>   
+            
         </div>
         
     );
