992030279b40978f28cdf45c8ce399a81c546ec4
Zinkeng <ajongafaczinkeng@gmail.com>
2022-08-21 00:11:32 +0100

    recent commit

--- src/Home.js
+++ src/Home.js
@@ -10,22 +10,23 @@ const Home = () => {
       </div>
       <div className="aboutpage">
         <div className="container">
-          <div className="image">
-            <img src={require("./Anne2.JPG")} alt="Zinkeng" />
+        <div  className="image">
+            <img src={require("./Anne3.JPG")} alt="Zinkeng" />
           </div>
 
           <div className="text">
           <h3 id="discover">Discover</h3>
-          <h4>About Me</h4>
+          <h4 style={{fontSize:"28px"}}>About Me</h4>
+          <hr style={{width: "60px"}}/>
             <p id="msg">
-              My name is Ajongafac Zinkeng Mbetem. I am a Software engineer{'\n'}
-              aspirant and graphic designer, based in Buea, Cameroon. I’m very
-              passionate and dedicated to my work. With my three years
-              experience in engineering school, I have acquired the skills
-              necessary to build great and premuim websites, applications and
-              softwares. I am particularly interested in open souce community
-              projects and projects that brings about innovation in the Cameroon
-              tech space and beyond.
+              My name is Ajongafac Zinkeng Mbetem. I am a <br /> Software engineer
+              aspirant and graphic designer,<br /> based in Buea, Cameroon. I’m very
+              passionate and<br /> dedicated to my work. With my three years
+              experience<br /> in engineering school, I have acquired the skills<br />
+              necessary to build great and premuim websites,<br />applications and
+              softwares. I am particularly interested<br />in open source community
+              projects and projects that<br /> brings about innovation in the Cameroon
+              tech space<br /> and beyond.
             </p>
           
 
@@ -39,8 +40,42 @@ const Home = () => {
               <li>Github link: https://github.com/Zinkeng</li>
             </ul>
           </div>
+          <button>Download CV</button>
           </div>
         </div>
+      </div>
+
+      <div className="services">
+      <h3 id="discover" style={{marginLeft:"100px", marginTop:"100px"}}>What I do</h3>
+          <h4 style={{fontSize:"28px", marginLeft:"100px"}}>My Services</h4>
+          <hr style={{width: "60px", marginLeft:"100px"}}/>
+        <div className="services-boxes">
+        <div className="box">
+          <div className="box-text">
+          <h3>Graphic Design</h3>
+          <p>Design flyers, posters, logos and branding</p>
+          </div>
+        </div>
+        <div className="box">
+          <div className="box-text">
+        <h3>User Interfaces</h3>
+          <p>Design user interfaces for both web and mobile applications</p>
+          </div>
+        </div>
+        <div className="box">
+        <div className="box-text">
+        <h3>Front-end web development</h3>
+          <p>Implement  front-end  web applications</p>
+          </div>
+        </div>
+        <div className="box">
+        <div className="box-text">
+        <h3>Mobile app development</h3>
+          <p>Create native mobile applications for both ios and android.</p>
+          </div>
+        </div>
+        </div>
+
       </div>
     </div>
   );
